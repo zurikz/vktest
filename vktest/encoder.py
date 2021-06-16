9c6d99f287c9a6d87d1eadae856fea04daee3813
Zurab Nurmagomedov <zurabnurmag@gmail.com>
2021-06-16 17:58:08 +0300

    Update encoder

--- vktest/encoder.py
+++ vktest/encoder.py
@@ -16,16 +16,14 @@ class EncoderConvBlock(nn.Module):
 			AgainConv1d(
 				in_channels=in_channels,
 				out_channels=out_channels,
-				kernel_size=3,
-				stride=1
+				kernel_size=3
 			),
 			nn.BatchNorm1d(out_channels),
 			nn.LeakyReLU(),
 			AgainConv1d(
 				in_channels=out_channels,
 				out_channels=in_channels,
-				kernel_size=3,
-				stride=1
+				kernel_size=3
 			)
 		)
 
@@ -40,53 +38,51 @@ class Encoder(nn.Module):
 		melbins (int): number of input channels.
 			In our terms it's the number of Mel bins.
 		melbins_out (int): number of output channels.
+		hidden_size (int): hidden size of channels.
 		conv_blocks_num (int): number of ConvBlocks in encoder.
-		seglen_out (int): length of segment after passing encoder.
-			It can be interpreted as width.
 	"""
 	def __init__(
 		self,
 		melbins: int,
 		melbins_out: int,
-		conv_blocks_num: int,	
-		seglen_out: int
+		hidden_size: int,
+		conv_blocks_num: int
 	) -> None:
 		super().__init__()
 		self.IN = InstanceNorm2d()
 		self.first_conv1d = AgainConv1d(
-			in_channels=melbins, out_channels=seglen_out
+			in_channels=melbins, out_channels=hidden_size
 		)
 		self.convblocks = nn.ModuleList(
 			conv_blocks_num * [
-				EncoderConvBlock(in_channels=seglen_out, out_channels=seglen_out)
+				EncoderConvBlock(in_channels=hidden_size, out_channels=hidden_size)
 			]
 		)
 		self.last_conv1d = AgainConv1d(
-			in_channels=seglen_out, out_channels=melbins_out
+			in_channels=hidden_size, out_channels=melbins_out
 		)
 	
 	def forward(self, x: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
 		"""Encoder's forward pass.
 
 		Args:
-			x (Tensor): (batch, 1, melbins, seglen)
+			x (Tensor): (batch, melbins, seglen)
 		
 		Returns:
 			tuple:	(encoder_out, means, stds)
 				encoder_out: (batch, melbins_out, seglen)
 		"""
-		# (B, 1, melbins, seglen) -> (B, seglen_out, seglen)
-		x = self.first_conv1d(x.squeeze(1))
+		# (B, melbins, seglen) -> (B, hidden_size, seglen)
+		x = self.first_conv1d(x)
 
 		means, stds = [], []
 		for block in self.convblocks:
 			x = block(x)
-			x, mean, std = self.IN(x.unsqueeze(1))
-			x = x.squeeze(1)
+			x, mean, std = self.IN(x)
 			means.append(mean)
 			stds.append(std)
 		
-		# (B, 1, seglen_out, seglen) -> (B, melbins_out, seglen)
-		x = self.last_conv1d(x.squeeze(1))
+		# (B, hidden_size, seglen) -> (B, melbins_out, seglen)
+		x = self.last_conv1d(x)
 
 		return x, means, stds
