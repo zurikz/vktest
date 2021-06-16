522a6664ccababf2444f603d98c8f6d0750cd59f
Zurab Nurmagomedov <zurabnurmag@gmail.com>
2021-06-17 02:18:53 +0300

    Add inference

--- vktest/model.py
+++ vktest/model.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 from torch import Tensor
+import torch.functional as F
+
 from .encoder import Encoder
 from .decoder import Decoder
 from .modules import CustomSigmoid
@@ -35,9 +37,8 @@ class AgainVC(nn.Module):
         Returns:
 
         """
-        seglen = source.shape[2]
-
         if target is None:
+            seglen = source.shape[2]
             target = torch.cat(
                 (source[:, :, (seglen // 2):], source[:, :, :(seglen // 2)]),
                 axis=2
@@ -50,3 +51,15 @@ class AgainVC(nn.Module):
         target_encoded = (self.sigmoid(target_content), target_means, target_stds)
 
         return self.decoder(source_content, target_encoded)
+
+    def inference(self, source: Tensor, target: Tensor) -> Tensor:
+        source_len, target_len = source.shape[2], target.shape[2]
+
+        if source_len % 8 != 0:
+            source = F.pad(source, (0, 8 - source_len % 8), mode='reflect')
+        if target_len % 8 != 0:
+            target = F.pad(target, (0, 8 - target_len % 8), mode='reflect')
+        
+        result = self.forward(source, target)
+
+        return result[:, :, :source_len]
