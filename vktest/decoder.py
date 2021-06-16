183f6aa827d48700579f7660e52969e16c29e923
Zurab Nurmagomedov <zurabnurmag@gmail.com>
2021-06-17 01:08:38 +0300

    Implemented Decoder

--- vktest/decoder.py
+++ vktest/decoder.py
@@ -1,8 +1,9 @@
+import torch
 from torch import nn
 from torch import Tensor
 from typing import Tuple
 
-from .modules import InstanceNorm2d, AgainConv1d
+from .modules import InstanceNorm, AgainConv1d
 
 
 class DecoderConvBlock(nn.Module):
@@ -59,15 +60,19 @@ class Decoder(nn.Module):
 
 		self.convblocks = nn.ModuleList(
 			conv_blocks_num * [
-				DecoderConvBlock(in_channels=hidden_size, out_channels=hidden_size)
+				DecoderConvBlock(
+					in_channels=hidden_size, 
+					out_channels=hidden_size
+				)
 			]
 		)
 
-		self.IN = InstanceNorm2d()
+		self.IN = InstanceNorm()
 		self.gru = nn.GRU(
 			input_size=hidden_size,
 			hidden_size=hidden_size,
-			num_layers=2
+			num_layers=2,
+			batch_first=True
 		)
 		self.linear = nn.Linear(hidden_size, melbins_out)
 	
@@ -82,7 +87,7 @@ class Decoder(nn.Module):
 			source (tuple): (content, means, stds)
 			target (tuple): (content, means, stds)
 				content (Tensor): (B, melbins, seglen)
-				means, stds (Tensor): (B, conv_blocks_num)
+				means, stds (Tensor): (conv_blocks_num, B, melbins, 1)
 		
 		Returns:
 		"""
@@ -91,19 +96,27 @@ class Decoder(nn.Module):
 
 		# Adaptive Instance Normalization
 		source_content, _, _ = self.IN(source_content)
-		_, target_content_mean, target_content_std = self.IN(target_content)
-		stylized_content = target_content_std * source_content + target_content_mean
+		_, target_mean, target_std = self.IN(target_content)
+		stylized_content = target_std * source_content + target_mean
 
 		# (B, melbins, seglen) -> (B, hidden_size, seglen)
 		stylized_content = self.first_conv1d(stylized_content)
 		output = self.leakyrelu(stylized_content)
 
-		# Flip tensors with means (and stds) to use
-		# means from the last layers of encoder first.
-		means, stds = means.flip(dims=[1]), stds.flip(dims=[1])
+		# Flip tensors of means (and stds) along "conv_blocks_num"
+		# to use means from the last layers of encoder first.
+		means, stds = means.flip(dims=[0]), stds.flip(dims=[0])
 		for (block, mean, std) in zip(self.convblocks, means, stds):
 			output = block(output)
 			# Adaptive Instance Normalization
 			output, _, _ = self.IN(output)
 			output = std * output + mean
-		
+
+		# output: (B, hidden_size, seglen)
+		output = torch.cat([means[-1], output], dim=2)
+		# (B, hidden_size, seglen) -> (B, seglen, hidden_size)
+		output = output.transpose(dim0=1, dim1=2)
+		output, _ = self.gru(output)
+		output = output[:, 1:, :]
+		output = self.linear(output)
+		return output.transpose(dim0=1, dim1=2)
